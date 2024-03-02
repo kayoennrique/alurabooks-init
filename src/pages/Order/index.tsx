c8c8d1508983cfad82c681f2ea4949d8c194a1f9
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-02 14:27:46 -0300

    feat(git): building the order checkout screen

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
+import PageSection from '../../components/PageSection';
+import { useHistory } from 'react-router-dom';
+
 const Order = () => {
+  const navigation = useHistory();
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div>
-      <h1>Order</h1>
-    </div>
+    <>
+      <PageSection>
+        <h1 className='text-2xl md:text-4xl text-white font-bold my-5'>Finalizar compra</h1>
+      </PageSection>
+      <div className='mt-20 w-full items-center justify-center flex'>
+        {!isLoading ? (
+          <img src='/loading.gif' alt='carregando' width={200} />
+        ) : (
+          <div className='flex-row justify-center'>
+            <h2 className='text-lg font-semibold'>
+              Tudo certo com seu pedido! Agradecemos pela compra
+            </h2>
+            <img src='/pedido_confirmado.webp' alt='Pedido confirmado' />
+            <button
+              className='w-full rounded-lg py-3 bg-[#EB9B00] text-white font-bold mt-4'
+              onClick={() => navigation.push('/')}
+            >
+              Voltar ao inicio
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
