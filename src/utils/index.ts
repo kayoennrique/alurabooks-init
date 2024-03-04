552d45e61281ca9435b58e295e90ae76e967d540
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-04 16:07:48 -0300

    feat(git): creating a function for application logs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,18 @@
+import { ErrorInfo } from 'react';
+
 export async function resolvePromise<T>(promise: Promise<T>): Promise<T> {
 	await new Promise((resolve) => {
 		setTimeout(resolve, 2000);
 	});
 	return await promise;
 }
+
+type ErrorLogFunction = (error: Error, errorInfo?: ErrorInfo) => void;
+
+export const errorLog: ErrorLogFunction = (error, errorInfo) => {
+	console.error('Erro capturado: ', error);
+
+	if (errorInfo) {
+		console.error('Informações adicionais: ', errorInfo);
+	}
+};
