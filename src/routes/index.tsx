2e517283295e7d2027c3a8481db1792e502ef05f
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-04 16:03:52 -0300

    feat(git): testing ErrorBoundary

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,24 @@
 import { Route, Switch } from 'react-router-dom';
-import Catalog from '../pages/Catalog';
-import BookDetail from '../pages/BookDetail';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { AppContext } from '../store/app';
+import ErroBoundary from '../components/ErroBoundary';
+import BookDetail from '../pages/BookDetail';
+import Catalog from '../pages/Catalog';
 import Order from '../pages/Order';
 import NotFound from '../pages/NotFound';
+import { AppContext } from '../store/app';
 
 export const Routes = () => (
-	<AppContext>
-		<Header />
-		<Switch>
-			<Route exact path='/' component={Catalog} />
-			<Route path='/book' component={BookDetail} />
-			<Route path='/order' component={Order} />
-			<Route path='*' component={NotFound} />
-		</Switch>
-		<Footer />
-	</AppContext>
+	<ErroBoundary>
+		<AppContext>
+			<Header />
+			<Switch>
+				<Route exact path='/' component={Catalog} />
+				<Route path='/book' component={BookDetail} />
+				<Route path='/order' component={Order} />
+				<Route path='*' component={NotFound} />
+			</Switch>
+			<Footer />
+		</AppContext>
+	</ErroBoundary>
 );
